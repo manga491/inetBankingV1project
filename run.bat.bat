a1da49edaa7cba824a49216bee013854fdbb0ff6
manga491 <mangasatoor@gmail.com>
2020-10-14 13:56:38 -0400

    This is my second commit

---
+++ run.bat.bat
@@ -0,0 +1,3 @@
+@echo off
+cd C:\Users\Venky\eclipse-workspace\inetbankingV1
+mvn clean install
---
+++ src/test/java/com/inetbanking/testcases/TC_LoginTest_001.java
@@ -0,0 +1,44 @@
+package com.inetbanking.testcases;
+
+import java.io.IOException;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import com.inetbanking.pageobjects.LoginPage;
+
+public class TC_LoginTest_001 extends BaseClass {
+
+	@Test
+	public void loginTest() throws IOException
+	{
+
+	driver.get(baseURL);
+	logger.info("URL is opened");
+	LoginPage lp=new LoginPage(driver);
+	lp.setUserName(username);
+	logger.info("username  Entered");
+	lp.setPassword(password);
+	logger.info("passdword Entered");
+	
+	lp.clickSubmit();
+	logger.info("click buttton Submitted");
+	
+	if(driver.getTitle().equals("Guru99 Bank Manager HomePage"))
+	{
+		Assert.assertTrue(true);
+		logger.info("Login test passed");
+	}
+	else
+	{
+		captureScreen(driver,"loginTest");
+		Assert.assertTrue(false);
+		logger.info("Login test failed");
+	}
+	}
+
+	
+		
+	}
+	
+
